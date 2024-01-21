995b4941ea4ef8e0f70278207153dc1206b8dccb
Zuhaili Amin <mohammadza.2020@scis.smu.edu.sg>
2024-01-21 23:56:49 +0800

    Added more routes
    Added more routes for CRUD-ing of books

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
@@ -57,6 +57,70 @@ app.get('/books', async (request, response) => {
         response.status(500).send({ message: error.message });
     }
 })
+
+// Getting 1 book by ID from mongoDB
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+// Updating a book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' })
+        };
+
+        return response.status(200).send({ messsage: 'Book updated successfully.'});
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+// Deleting a book
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' })
+        }
+
+        return response.status(200).send({ messsage: 'Book deleted successfully.'});
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
 
 
 // Connecting to mongoDB
