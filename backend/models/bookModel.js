824bef385776d40ea43b36606da053eeb14dd003
Zuhaili Amin <mohammadza.2020@scis.smu.edu.sg>
2024-01-21 23:14:00 +0800

    Starting off the project
    Creating index.js for the main page, models to create different objects, package.json was created automatically to specify certain packages

---
+++ backend/models/bookModel.js
@@ -0,0 +1,25 @@
+// creating a Book Model
+
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
